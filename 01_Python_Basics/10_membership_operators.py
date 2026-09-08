f7d9f89b1f90e4901a39ac07596bb7603bda96df
Harshi060 <thamaralaharshitha@gmail.com>
2026-09-08 21:09:07 +0530

    Create 10_membership_operators.py

---
+++ 01_Python_Basics/10_membership_operators.py
@@ -0,0 +1,11 @@
+# Membership Operators
+
+fruits = ["apple", "banana", "mango"]
+
+# Using in
+print("apple" in fruits)
+print("orange" in fruits)
+
+# Using not in
+print("orange" not in fruits)
+print("banana" not in fruits)
