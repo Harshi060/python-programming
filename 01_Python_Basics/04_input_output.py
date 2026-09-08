6edd395f7707975f8e0ef5a94ff7457c83b5ba4d
Harshi060 <thamaralaharshitha@gmail.com>
2026-09-08 20:15:11 +0530

    Create 04_input_output.py

---
+++ 01_Python_Basics/04_input_output.py
@@ -0,0 +1,11 @@
+# Taking input from the user
+
+name = input("Enter your name: ")
+college = input("Enter your college: ")
+course = input("Enter your course: ")
+
+# Displaying output
+
+print("Name:", name)
+print("College:", college)
+print("Course:", course)
