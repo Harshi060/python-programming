b39452201c35d40be1abec75d2cff21dde747d85
Harshi060 <thamaralaharshitha@gmail.com>
2026-09-08 21:39:17 +0530

    Add script to check if a number is a multiple of 10

---
+++ 01_Python_Basics/22_multiple_of_10.py
@@ -0,0 +1,8 @@
+# Check whether a number is a multiple of 10
+
+number = int(input("Enter a number: "))
+
+if number % 10 == 0:
+    print("Multiple of 10")
+else:
+    print("Not a multiple of 10")
