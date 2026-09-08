60d3e4a758794ce05fdb20d8cf69d1e67b1298f8
Harshi060 <thamaralaharshitha@gmail.com>
2026-09-08 21:02:46 +0530

    Create 09_assignment_operators.py

---
+++ 01_Python_Basics/09_assignment_operators.py
@@ -0,0 +1,26 @@
+# Assignment Operators
+
+a = 10
+
+print("Initial value:", a)
+
+a += 5
+print("After +=:", a)
+
+a -= 3
+print("After -=:", a)
+
+a *= 2
+print("After *=:", a)
+
+a /= 2
+print("After /=:", a)
+
+a //= 2
+print("After //=:", a)
+
+a %= 3
+print("After %=:", a)
+
+a **= 2
+print("After **=:", a)
