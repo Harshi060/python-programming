00f35e74f22da233f54b57fd2c48ba3c71c970f1
Harshi060 <thamaralaharshitha@gmail.com>
2026-09-08 20:08:07 +0530

    Create 03_data_types.py

---
+++ 01_Python_Basics/03_data_types.py
@@ -0,0 +1,20 @@
+# Integer
+age = 20
+
+# Float
+percentage = 94.5
+
+# String
+name = "Harshi"
+
+# Boolean
+is_student = True
+
+# Complex
+number = 2 + 3j
+
+print(age)
+print(percentage)
+print(name)
+print(is_student)
+print(number)
